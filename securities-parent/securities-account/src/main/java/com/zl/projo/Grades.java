2ecabc0cb6b9c013cbfa1c52bd04ae4dfb02abf3
Administrator <Administrator@LX>
2019-06-05 16:50:54 +0800

    添加分数类

---
+++ securities-parent/securities-account/src/main/java/com/zl/projo/Grades.java
@@ -0,0 +1,122 @@
+package com.zl.projo;
+
+public class Grades {
+private Integer radio1=1;
+private Integer radio2=1;
+private Integer radio3;
+private Integer radio4;
+private Integer radio5;
+private Integer radio6;
+private Integer radio7;
+private Integer radio8;
+private Integer radio9;
+private Integer radio10;
+private Integer radio11;
+private Integer radio12;
+private Integer radio13;
+private Integer radio14;
+private Integer radio15;
+private Integer allGrades;
+public Integer getRadio1() {
+	return radio1;
+}
+public void setRadio1(Integer radio1) {
+	this.radio1 = radio1;
+}
+public Integer getRadio2() {
+	return radio2;
+}
+public void setRadio2(Integer radio2) {
+	this.radio2 = radio2;
+}
+public Integer getRadio3() {
+	return radio3;
+}
+public void setRadio3(Integer radio3) {
+	this.radio3 = radio3;
+}
+public Integer getRadio4() {
+	return radio4;
+}
+public void setRadio4(Integer radio4) {
+	this.radio4 = radio4;
+}
+public Integer getRadio5() {
+	return radio5;
+}
+public void setRadio5(Integer radio5) {
+	this.radio5 = radio5;
+}
+public Integer getRadio6() {
+	return radio6;
+}
+public void setRadio6(Integer radio6) {
+	this.radio6 = radio6;
+}
+public Integer getRadio7() {
+	return radio7;
+}
+public void setRadio7(Integer radio7) {
+	this.radio7 = radio7;
+}
+public Integer getRadio8() {
+	return radio8;
+}
+public void setRadio8(Integer radio8) {
+	this.radio8 = radio8;
+}
+public Integer getRadio9() {
+	return radio9;
+}
+public void setRadio9(Integer radio9) {
+	this.radio9 = radio9;
+}
+public Integer getRadio10() {
+	return radio10;
+}
+public void setRadio10(Integer radio10) {
+	this.radio10 = radio10;
+}
+public Integer getRadio11() {
+	return radio11;
+}
+public void setRadio11(Integer radio11) {
+	this.radio11 = radio11;
+}
+public Integer getRadio12() {
+	return radio12;
+}
+public void setRadio12(Integer radio12) {
+	this.radio12 = radio12;
+}
+public Integer getRadio13() {
+	return radio13;
+}
+public void setRadio13(Integer radio13) {
+	this.radio13 = radio13;
+}
+public Integer getRadio14() {
+	return radio14;
+}
+public void setRadio14(Integer radio14) {
+	this.radio14 = radio14;
+}
+public Integer getRadio15() {
+	return radio15;
+}
+public void setRadio15(Integer radio15) {
+	this.radio15 = radio15;
+}
+public Integer getAllGrades() {
+	return radio1+radio2+radio3+radio4+radio5+radio6+radio7+radio8+radio9+radio10
+			+radio11+radio12+radio13+radio14+radio15;
+}
+@Override
+public String toString() {
+	return "Grades [radio1=" + radio1 + ", radio2=" + radio2 + ", radio3=" + radio3 + ", radio4=" + radio4 + ", radio5="
+			+ radio5 + ", radio6=" + radio6 + ", radio7=" + radio7 + ", radio8=" + radio8 + ", radio9=" + radio9
+			+ ", radio10=" + radio10 + ", radio11=" + radio11 + ", radio12=" + radio12 + ", radio13=" + radio13
+			+ ", radio14=" + radio14 + ", radio15=" + radio15 + ", allGrades=" + allGrades + "]";
+}
+
+}
